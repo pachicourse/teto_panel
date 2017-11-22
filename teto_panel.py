783250b7fd87b318f019140347193680ab970ad4
pachicourse <ddds78199500825@gmail.com>
2017-11-22 07:59:20 +0000

    1.0

--- teto_panel.py
+++ teto_panel.py
@@ -1,7 +1,40 @@
 import subprocess
-def test():
+import wiringpi as wp
+import time
+
+RIGHT_ARM_PIN = 5
+LEFT_ARM_PIN = 6
+RIGHT_LEG_PIN = 13
+LEFT_LEG_PIN = 19
+HEAD_PIN = 26
+
+def pin_setup():
+    wp.wiringPiSetupGpio()
+    wiringpi.pinMode(RIGHT_ARM_PIN, 0)
+    wiringpi.pinMode(LEFT_ARM_PIN, 0)
+    wiringpi.pinMode(RIGHT_LEG_PIN, 0)
+    wiringpi.pinMode(LEFT_LEG_PIN, 0)
+    wiringpi.pinMode(HEAD_PIN, 0)
+
+def check_tilt_and_aplay():
+    # must be pull-down
+    if(wp.digitalRead(RIGHT_ARM_PIN) == 1):
+        subprocess.call("aplay voice/test.wav", shell=True)
+    if(wp.digitalRead(LEFT_ARM_PIN_LEG_PIN) == 1):
+        subprocess.call("aplay voice/test.wav", shell=True)
+    if(wp.digitalRead(RIGHT_LEG_PIN) == 1):
+        subprocess.call("aplay voice/test.wav", shell=True)
+    if(wp.digitalRead(LEFT_LEG_PIN) == 1):
+        subprocess.call("aplay voice/test.wav", shell=True)
+    if(wp.digitalRead(HEAD_PIN) == 1):
+        subprocess.call("aplay voice/test.wav", shell=True)
+
+def test_voice():
     subprocess.call("aplay voice/test.wav", shell=True)
 
 if __name__ == '__main__':
-    test()
-
+    test_voice()
+    pin_setup()
+    while True:
+        check_tilt_and_aplay()
+        time.sleep(0.5)
