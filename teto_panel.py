638ad6d22c1d5b1a109aa77d2fe0193accb67bfb
pachicourse <ddds78199500825@gmail.com>
2017-11-24 12:29:12 +0900

    rename wav

--- teto_panel.py
+++ teto_panel.py
@@ -19,15 +19,20 @@ def pin_setup():
 def check_tilt_and_aplay():
     # must be pull-down
     if(wp.digitalRead(RIGHT_ARM_PIN) == 1):
-        subprocess.call("aplay voice/test.wav", shell=True)
+        subprocess.call("aplay voice/right_arm.wav", shell=True)
+        return
     if(wp.digitalRead(LEFT_ARM_PIN_LEG_PIN) == 1):
-        subprocess.call("aplay voice/test.wav", shell=True)
+        subprocess.call("aplay voice/left_arm.wav", shell=True)
+        return
     if(wp.digitalRead(RIGHT_LEG_PIN) == 1):
-        subprocess.call("aplay voice/test.wav", shell=True)
+        subprocess.call("aplay voice/right_leg.wav", shell=True)
+        return
     if(wp.digitalRead(LEFT_LEG_PIN) == 1):
-        subprocess.call("aplay voice/test.wav", shell=True)
+        subprocess.call("aplay voice/left_leg.wav", shell=True)
+        return
     if(wp.digitalRead(HEAD_PIN) == 1):
-        subprocess.call("aplay voice/test.wav", shell=True)
+        subprocess.call("aplay voice/head.wav", shell=True)
+        return
 
 def test_voice():
     subprocess.call("aplay voice/test.wav", shell=True)
